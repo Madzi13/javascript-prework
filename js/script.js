8e2dc7049d2fea159c4c04b045ab6f9d08177d07
Magdalena Stryszowska <madzi13@op.pl>
2023-06-05 09:54:21 +0200

    zapisanie odpowiedzi gracza

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-let computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}else if(playerInput == '2'){
+  playerMove = 'papier';
+}else if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
