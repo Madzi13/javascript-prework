7311bf2c46321595a12a6acb876943eb526f6abc
Magdalena Stryszowska <madzi13@op.pl>
2023-06-06 19:34:21 +0200

    Wynik gry

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}else if(randomNumber == 2){
+  computerMove = 'papier';
+}else if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -9,6 +27,28 @@ if(playerInput == '1'){
 }else if(playerInput == '2'){
   playerMove = 'papier';
 }else if(playerInput == '3'){
-    playerMove = 'nożyce';
+  playerMove = 'nożyce';
 }
 printMessage('Twój ruch to: ' + playerMove);
+
+
+if( computerMove == 'kamień' && playerMove == 'papier' ){
+  printMessage('Ty wygrywasz!');
+}else if( computerMove == 'nożyczki' && playerMove == 'kamień' ){
+  printMessage('Ty przegrywasz!');
+}else if( computerMove == 'papier' && playerMove == 'nożyczki' ){
+  printMessage('Ty wygrywasz!');
+}else if( computerMove == 'kamień' && playerMove == 'nożyczki' ){
+  printMessage('Ty przegrywasz!');
+}else if( computerMove == 'papier' && playerMove == 'kamień' ){
+  printMessage('Ty wygrywasz!');
+}
+
+
+else if( computerMove == 'kamień' && playerMove == 'kamień' ){
+  printMessage('Remis');
+}else if( computerMove == 'papier' && playerMove == 'papier' ){
+  printMessage('Remis');
+}else if( computerMove == 'nożyce' && playerMove == 'nożyce' ){
+  printMessage('Remis');
+}
